c1774fd430035940fc327ab562c65c97710e1753
cnnrshd <cws4197@g.rit.edu>
2018-04-20 22:41:59 -0400

    fixed AD

--- scripts/5minAD.ps1
+++ scripts/5minAD.ps1
@@ -21,18 +21,19 @@ mkdir C:\tempDL
 Start-Service BITS
 # download and run the firewall script, make sure to set ExecutionPolicy to Unrestricted (only for that line)
 Import-Module BitsTransfer
-$firewallURL = "https://raw.githubusercontent.com/cnnrshd/irsec_scripts/master/scripts/firewallRules.ps1?token=AiUZH4U175nUYsPanhLvY6qu722KXJpKks5a4Sx_wA%3D%3D"
+Import-Module ActiveDirectory
+$firewallURL = "https://gist.githubusercontent.com/cnnrshd/c0d435c0f14e523d9faa38e2702c2c53/raw/d066980f97cb185afa586c2c39fc737dddc7d2a4/firewallRules.ps1"
 $firewallOUT = "C:\scripts\firewallRules.ps1"
 Start-BitsTransfer -Source $firewallURL -Destination $firewallOUT
 Start-Job $firewallOUT
 # Start download of config scripts
 # Grab/Run Packetbeat install script
-$packetURL = "https://raw.githubusercontent.com/cnnrshd/irsec_scripts/master/scripts/install_packetbeat.ps1?token=AiUZH0wDQUGO9lYUyDFAm4QHulgRndDBks5a4S4zwA%3D%3D"
+$packetURL = "https://gist.githubusercontent.com/cnnrshd/02839b9403431567a1dc176caa63d2b8/raw/398d4b84ed7f48f0a1c25268c891be107caf0a59/install_packbeat.ps1"
 $packetOUT = "C:\scripts\install_packetbeat.ps1"
 Start-BitsTransfer -Source $packetURL -Destination $packetOUT
 Start-Job $packetOUT
 # Grab/Run Winlogbeat install script
-$winbeatURL = "https://raw.githubusercontent.com/cnnrshd/irsec_scripts/master/scripts/install_winlogbeat.ps1?token=AiUZHxk66PKkustrb1GsZRAGcXIjqufKks5a4S46wA%3D%3D"
+$winbeatURL = "https://gist.githubusercontent.com/cnnrshd/6f48a8e002068fe3a1a335f2a473c72c/raw/58960043219a2393893aa6d14544445a82ce3e12/install_winlogbeat.ps1"
 $winbeatOUT = "C:\scripts\install_winlogbeat.ps1"
 Start-BitsTransfer -Source $winbeatURL -Destination $winbeatOUT
 Start-Job $winbeatOUT
@@ -42,11 +43,12 @@ Start-Job $winbeatOUT
 #Start-BitsTransfer -Source $filebeatURL -Destination $filebeatOUT
 #Start-Job $filebeatOUT
 # Grab/Run Sysinternals/sysmon install script
-$sysintURL = "https://raw.githubusercontent.com/cnnrshd/irsec_scripts/master/scripts/install_sysinternals.ps1?token=AiUZHwTTdqOVOZmn-IoxYz1scyQzH3qEks5a4S42wA%3D%3D"
+$sysintURL = "https://gist.githubusercontent.com/cnnrshd/e8a74f217bf9f0a775b96233bdecf8bc/raw/d0ddd37019619924a06c871e2bca5894cfb47d6b/install_sysinternals.ps1"
 $sysintOUT = "C:\scripts\install_sysinternals.ps1"
 Start-BitsTransfer -Source $sysintURL -Destination $sysintOUT
 Start-Job $sysintOUT
 # Download kerberos reset script
 $krbURL = "https://gallery.technet.microsoft.com/Reset-the-krbtgt-account-581a9e51/file/142338/1/New-CtmADKrbtgtKeys.zip"
 $krbOUT = "C:\tempDL\New-CtmADKrbtgtKeys.zip"
+Start-BitsTransfer -Source $krbURL -Destination $krbOUT
 Expand-Archive -LiteralPath "C:\tempDL\New-CtmADKrbtgtKeys.zip" -DestinationPath "C:\New-CtmADKrbtgtKeys"
